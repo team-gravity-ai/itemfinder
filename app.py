6d0baf83aad4c4d09eb6325932f520c5055866e3
ronen hong <ronen.hong@gmail.com>
2022-08-07 17:58:13 +0900

    해상도 확인용 ui

---
+++ app.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+from PyQt5 import uic
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon, QRegExpValidator, QTextCursor
+
+
+main_ui           = uic.loadUiType(os.path.join("ui", "main.ui"))[0] 
+
+class MyApp(QMainWindow, main_ui):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setFixedSize(873, 709)
+
+
+
+
+
+if __name__ == "__main__" :
+
+    
+    
+    app = QApplication(sys.argv)
+    
+    myApp = MyApp()
+    
+    myApp.show()
+    
+    app.exec_()
